1a34112291de1b949a590b869528296762a72c95
Fabrizzio20k <fabrizzio785@gmail.com>
2026-05-13 02:39:09 -0500

    FIX: Importation of sh files on install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# Importamos colores
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-BLUE='\033[0;34m'
-NC='\033[0m'
+source ./scripts/common.sh
 
 if [ "$EUID" -ne 0 ]; then
     echo -e "${RED}[!] Por favor, ejecuta este script como root (sudo bash install.sh)${NC}"
